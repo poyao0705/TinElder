0796962995c18664889192fab6c11ee033ac2638
Mila PChu <uchu0676@uni.sydney.edu.au>
2024-08-24 22:26:26 +1000

    added MyEvents

---
+++ FrontEnd/src/components/MyEvents.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MyEvents = () => {
+  return (
+    <div className="h-[3357px] opacity-5 justify-between items-start inline-flex">
+      <div className="w-[3357px] h-[0px] origin-top-left -rotate-90 border border-black"></div>
+      <div className="w-[3357px] h-[0px] origin-top-left -rotate-90 border border-black"></div>
+      <div className="w-[3357px] h-[0px] origin-top-left -rotate-90 border border-black"></div>
+      <div className="w-[3357px] h-[0px] origin-top-left -rotate-90 border border-black"></div>
+      <div className="w-[3357px] h-[0px] origin-top-left -rotate-90 border border-black"></div>
+    </div>
+  );
+};
+
+export default MyEvents;
