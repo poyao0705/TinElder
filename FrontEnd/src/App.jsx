61a010f615699edd4be19ee601520ad3f02726c4
Hanxi <sissy_guohanxi@163.com>
2024-08-24 23:42:01 +1000

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainContent from './components/MainContent';
-import RegisterPage from './components/RegisterPage';
 import LoginPage from './components/LoginPage';
+<<<<<<< HEAD
 import HomePage from './components/HomePage'; 
 import Music from './components/music';  // Import Music component
 import MyEvents from './components/MyEvents';
 import './index.css';
+=======
+import RegisterPage from './components/RegisterPage';
+import MyEvents from './components/MyEvents';
+>>>>>>> 9de041e1e2b91af6e88ac63848429d520b294ad5
 
-const App = () => {
+function App() {
   return (
+<<<<<<< HEAD
     <div className="app-background">
       <Router>
         <Routes>
@@ -22,7 +27,17 @@ const App = () => {
         </Routes>
       </Router>
     </div>
+=======
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/myEvent" element={<MyEvents />} />
+      </Routes>
+    </Router>
+>>>>>>> 9de041e1e2b91af6e88ac63848429d520b294ad5
   );
-};
+}
 
 export default App;
