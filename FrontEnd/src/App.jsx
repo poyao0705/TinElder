9de041e1e2b91af6e88ac63848429d520b294ad5
Mila PChu <uchu0676@uni.sydney.edu.au>
2024-08-24 23:33:43 +1000

    save myEvent

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainContent from './components/MainContent';
-import RegisterPage from './components/RegisterPage';
 import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage'; // Import the HomePage component
-import './index.css';
+import RegisterPage from './components/RegisterPage';
+import MyEvents from './components/MyEvents';
 
-const App = () => {
+function App() {
   return (
-    <div className="app-background">
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<HomePage />} />  {/* Add the home route */}
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/myEvent" element={<MyEvents />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
